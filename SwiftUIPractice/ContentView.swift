792441c82ca9558d92e42791f258224faf81e628
Jin0331 <sempre813@naver.com>
2024-04-17 23:21:09 +0900

    탭바 및 폴더정리

--- SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/ContentView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            
+          FirstView()
+            .tabItem {
+              Image(systemName: "1.square.fill")
+            }
+          SecondView()
+            .tabItem {
+              Image(systemName: "2.square.fill")
+            }
+          ThirdView()
+            .tabItem {
+              Image(systemName: "3.square.fill")
+            }
         }
-    }
+      }
+    
 }
 
 #Preview {
